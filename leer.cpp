c346d7b732face58fed90155135dcb456b0641fe
AbrahamGarcia22 <42215202+AbrahamGarcia22@users.noreply.github.com>
2020-03-02 13:29:36 -0600

    Add files via upload
    Primer prueba de lectura

---
+++ leer.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int contador(string &cont,char c){
+string::const_iterator i;
+int conta=0;
+int flag =0;
+char flak=' ';
+for(i=cont.begin(); i!=cont.end(); ++i){
+    if(*i==c||*i=='='||*i=='-'||*i=='<'||*i=='>'||*i=='['){
+        if (flag==0||(flak!=*i&&flag==1)){
+            ++conta;
+            flak=*i;
+            flag=1;
+        }
+        else{
+            flag=0;
+        }
+    }
+    else{
+        flag=0;
+    }
+}
+    if(cont.find("return")!=string::npos||cont.find("&&")!=string::npos||cont.find("||")!=string::npos){
+        ++conta;
+    }
+    return conta;
+};
+string leer(ifstream &ref){
+    int cont=1;
+    string s, contenido;
+    while(getline(ref,s)){
+        contenido +=s;
+        cout<<contenido<<" OE: "<<contador(contenido,'+')<<endl;
+        contenido="";
+    }
+};
+
+int main(){
+    int cantidad;
+    string contenido;
+    ifstream archivo;
+    archivo.open("/home/rojo/Escritorio/texto.txt");
+    contenido= leer(archivo);
+    archivo.close();
+
+    
+}
+
+
